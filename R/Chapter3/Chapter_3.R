927c4642b435bd26ad45611a5b09170f22d7f574
Akhapwoya Obuchere <thechristopheao@gmail.com>
2025-01-05 20:41:57 +0300

    Completed multiple linear regression

--- R/Chapter3/Chapter_3.R
+++ R/Chapter3/Chapter_3.R
@@ -74,7 +74,178 @@ t_beta_0; t_beta_1
 TSS = sum(advert_2$`y-y_bar`^2)
 R_SQUARED = 1 - (RSS/TSS)
 R_SQUARED
+# 
+# Regression
 #
+lin_model <- function(formula, dat){
+  
+  # Ensure data is a dataframe
+  if (class(dat) != "data.frame"){
+    stop("Data must be input in the data.frame format")
+  }
+  
+  # All character to factor and integers to numeric
+  dat[sapply(dat, is.character)] <- lapply(dat[sapply(dat, is.character)], 
+                                           as.factor)
+  dat[sapply(dat, is.integer)] <- lapply(dat[sapply(dat, is.integer)], 
+                                         as.numeric)
+  
+  # get all independent variables and response variable from formula
+  ind_resp_var <- unique(all.vars(formula))
+  if (ind_resp_var[2] == ".") {ind_resp_var <- c(ind_resp_var[1], colnames(dat)[colnames(dat) != ind_resp_var[1]])}
+  
+  # check that independent variables are either numeric or factor
+  class_pred <- sapply(ind_resp_var[-1], function(x) {class(dat[, x])}) #get classes
+  class_pred_unique <- unique(class_pred) #unique clases
+  
+  #comparison of class and checks
+  if(!any(class_pred %in% c("factor", "numeric", "single", "double"))) {
+    stop("Independent variables should be of numeric or factor types")
+  }
+  
+  # model matrix x and y variable
+  X <- model.matrix(formula,dat)
+  y <- ind_resp_var[1] 
+  #
+  n_obs = nrow(X)
+  p_vars = ncol(X)
+  
+  #
+  # check that dependent variable is numeric
+  if (!is.numeric(dat[, ind_resp_var[1]])){
+    stop("Dependent variable should be numeric type")}###
+  #
+  cl <- match.call()
+  #'Create class
+  lnreg <- list()
+  class(lnreg) <- "linmod"
+  
+  # Factor Elements
+  factor_indep_vars <- names(class_pred[class_pred == "factor"])
+  lnreg$flev <- lapply(factor_indep_vars, function(x) {levels(dat[, x])})
+  names(lnreg$flev) <- factor_indep_vars
+  #
+  mu_y = mean(dat[,ind_resp_var[1]], na.rm = T)
+  # y element
+  lnreg$y <- dat[,ind_resp_var[1]]
+  # y name
+  lnreg$yname <- ind_resp_var[1]
+  #'Decomposing 
+  qr_x_m <- qr(X)
+  # Get Regression coefficient
+  lnreg$beta <- solve(qr.R(qr_x_m)) %*% t(qr.Q(qr_x_m)) %*% dat[,y]
+  # Alternative below provide inefficient estimates to the data
+  #lnreg$beta <- (qr.solve(qr.R(qr_x_m))*(qr.R(qr_x_m))) %*% t(qr.Q(qr_x_m)) %*% dat[,y]
+  # fitted values
+  lnreg$mu <- qr.fitted(qr_x_m,dat[,y])
+  # residual values
+  lnreg$residu <- qr.resid(qr_x_m,dat[,y])
+  # Degrees of freedom
+  lnreg$degfre <- nrow(dat)-ncol(X)
+  
+  # Gives residual variance abd regression coeff vars
+  lnreg$sigma <- (t(lnreg$residu) %*% lnreg$residu) /lnreg$degfre
+  #'converting into a scalar quantity
+  resid_var_sc <- lnreg$sigma[1,1]
+  regcoemat1 <- chol2inv(qr.R(qr_x_m))
+  regcoemat2 <- vector(length = nrow(regcoemat1))
+  for (i in 1:nrow(regcoemat1)) {
+    regcoemat2[i] <- regcoemat1[i,i]
+  }
+  # variance of Regression coefficients
+  lnreg$V <- regcoemat2 * resid_var_sc
+  
+  #'T-values for each coefficient
+  tvalue <- vector(length = nrow(lnreg$beta))
+  for (i in 1:nrow(lnreg$beta)) {
+    tvalue[i] <- lnreg$beta[i,1]/sqrt(lnreg$V[i])
+  }
+  lnreg$tval <- tvalue
+  #'P-values
+  lnreg$pval <- pt(abs(lnreg$tval),lnreg$degfre, lower.tail = FALSE)
+  lnreg$formula <- cl #model formula
+  # model F value .... need to look into it
+  lnreg$F_value = (sum((lnreg$y - mu_y)^2) -  sum(lnreg$residu^2) )/(sum(lnreg$residu^2)/(n_obs - p_vars - 1)) 
+  return(lnreg)
+}
+#
+# Advertising
+model_TV_sales <- lin_model(formula = sales ~ TV,dat = advertising)
+model_radio_sales <- lin_model(formula = sales ~ radio,dat = advertising)
+model_newspaper_sales <- lin_model(formula = sales ~ newspaper,dat = advertising)
+model_allpred_sales <- lin_model(formula = sales ~ TV+radio+newspaper,dat = advertising)
+#
+model_TV_sales$beta
+model_radio_sales$beta
+model_newspaper_sales$beta
+model_allpred_sales$beta
+#
+predict.lin_model <- function(x, newdata){
+  call_forme = regmatches(deparse(x$formula), regexec("=\\s*(.*?)\\s*," , deparse(x$formula)))[[1]][2]
+  ind_resp_var <- unique(all.vars(eval(parse(text=call_forme))))
+  #
+  print(names(newdata))
+  #
+  if (ind_resp_var[1] %in% names(newdata)){
+    newdata = newdata
+  } else {
+    y_name <- ind_resp_var[1]
+    newdata[y_name] <- rep(0, nrow(newdata))
+  }
+  
+  X <- model.matrix(eval(parse(text=call_forme)),newdata)
+  predictions <- X %*% c(x$beta)
+  predictions
+}
 
+predict.lin_model(model_allpred_sales, newdata = advertising)
+predict.lin_model(model_TV_sales, newdata = advertising[,1, drop=F])
+
+
+# Print Function
+print.lin_model <- function(x){
+  cat(regmatches(deparse(x$formula), regexec("=\\s*(.*?)\\s*," , deparse(x$formula)))[[1]][2])
+  cat("\n\n") 
+  if(length(x$beta[,1])){
+    cat("Coefficients:\n")
+    # the output has 2 columns, all are numeric
+    stderror <- sqrt(x$V)
+    estder <- cbind(x$beta,stderror)
+    colnames(estder) <- c("Estimate", "Std. Error")
+    print(estder)
+  }
+  else cat("No coefficients\n")
+  invisible(x)
+}
+
+print.lin_model(model_allpred_sales)
+
+# Plot Funtion
+plot.lin_model <- function(x){
+  data1 <- cbind(x$mu,x$residu)
+  data1 <- as.data.frame(data1)
+  names(data1) <- c("mu","residu")
+  plot(x = data1$mu, y= data1$residu, 
+       xlab = "Fitted values", ylab = "Residuals",
+       main = "Residuals vs Fitted")
+  abline(0, 0)                  # error
+  grid()
+}
+
+plot.lin_model(model_allpred_sales)
 #
+# multiple reg lm
+model_allpred_vs_sales <- lm(formula = sales ~ TV+radio+newspaper,dat = advertising)
+summary(model_allpred_vs_sales)
+#
+cor(advertising)
+#
+pairs(Credit)
+#
+model_credit = lm(Balance~Own, data = Credit)
+summary(model_credit)
+#
+lin_model(Balance~Own, dat = Credit)$beta
+print.lin_model(lin_model(Balance~Own, dat = Credit))
+lin_model(Balance~Region, dat = Credit)$beta
 #
