94989f808978ae19e18c5d72b4696a69906af0ac
Akhapwoya Obuchere <thechristopheao@gmail.com>
2025-02-15 23:51:39 +0300

    Ch4

--- R/Chapter4/chapter_4.R
+++ R/Chapter4/chapter_4.R
@@ -13,3 +13,44 @@ par(mfrow = c(1,1))
 #
 # 4.3 Logistic Regression
 #
+table(Default$default)
+
+## 4.3.1 The logistic model
+default_balance_model <- glm(
+  formula = default~balance, family = binomial(),
+  data = Default)
+summary(default_balance_model)
+predict(
+  default_balance_model, newdata = data.frame(balance = c(1000, 2000)), 
+  type = "response")
+#
+default_student_model <- glm(
+  formula = default~student, family = binomial(),
+  data = Default)
+summary(default_student_model)
+predict(default_student_model, 
+        newdata = data.frame(student = c("Yes","No")), 
+        type = "response")
+#
+## 4.3.4
+#
+multiple_model <- glm(
+  formula = default~balance+income+student, family = binomial(),
+  data = Default)
+summary(multiple_model)
+#
+pred_bal = predict(
+  default_balance_model, newdata = data.frame(balance = c(min(Default$balance):max(Default$balance))), 
+  type = "response")
+pred_bal
+#
+
+balance_data = data.frame(
+  balance = c(min(Default$balance):max(Default$balance))
+) |> cbind(pred_bal)
+head(balance_data)
+#
+plot(pred_bal~balance, data = balance_data, )
+#
+# 4.3.5 
+#
